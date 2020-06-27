f941c273cbd8ebde285960f5e99b9a354f1b89a7
byungkyu kim <eyekiller@gmail.com>
2020-06-28 07:30:03 +0900

    add config

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://bkkim:@catchstar-byn9b.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() =>  console.log('MongoDb connected'))
 .catch(err => console.log(err))
 
-// mongodb+srv://bkkim:<password>@catchstar-byn9b.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! 테스트'))
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body);
+    user.save((err, doc)=> {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    });
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
